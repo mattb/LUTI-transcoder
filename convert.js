a3ff376348fbaa9917abe5244cc61c412d6e5143
Matt Biddulph <mbiddulph@apple.com>
2019-11-29 23:18:27 -0800

    Updated ID for preset

--- convert.js
+++ convert.js
@@ -2,7 +2,7 @@
 const aws = require('aws-sdk');
 
 const PipelineId = '1548039311520-hhiw1e';
-const PresetId = '1569302061448-7wrs9a';
+const PresetId = '1575096974967-o3cmgx';
 
 module.exports = (fileKey, callback) => {
   const elastictranscoder = new aws.ElasticTranscoder({ region: 'us-east-1' });

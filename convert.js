a0bf50bb8e8958c1ade75a0ecfc669e70d20b2bc
Matt Biddulph <mbiddulph@apple.com>
2019-12-04 21:54:50 -0800

    Expect HLS playlists to encode 4M/2M/200k multi streams

--- convert.js
+++ convert.js
@@ -2,7 +2,11 @@
 const aws = require('aws-sdk');
 
 const PipelineId = '1548039311520-hhiw1e';
-const PresetId = '1575096974967-o3cmgx';
+const PresetIds = [
+  '1575139606514-gdm3bw',
+  '1575431325080-s3xrem',
+  '1575434396298-50o34r'
+];
 
 module.exports = (fileKey, callback) => {
   const elastictranscoder = new aws.ElasticTranscoder({ region: 'us-east-1' });
@@ -12,23 +16,22 @@ module.exports = (fileKey, callback) => {
   };
 
   const outputKey = fileKey.replace(/\.mp4$/, '');
-  const Output = {
-    PresetId,
-    SegmentDuration: '1.0',
-    Key: outputKey,
-    ThumbnailPattern: fileKey.replace(/\.mp4$/, '-{count}')
-  };
+  const Outputs = PresetIds.map((pid, idx) => ({
+    SegmentDuration: '2.0',
+    PresetId: pid,
+    Key: `${outputKey}-${idx}`
+  }));
 
   const Playlists = [
     {
       Format: 'HLSv4',
       Name: `${outputKey}-playlist`,
-      OutputKeys: [outputKey]
+      OutputKeys: Outputs.map(o => o.Key)
     }
   ];
 
   elastictranscoder.createJob(
-    { PipelineId, Input, Output, Playlists },
+    { PipelineId, Input, Outputs, Playlists },
     callback
   );
 };
